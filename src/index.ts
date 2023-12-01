0fbf2606e19c3db593ee265e1ce1ee138ff866a2
georgesycabaccang <georgesycabaccang@gmail.com>
2023-12-01 15:56:00 +0800

    Add env for cors origin

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ database.once("open", () => console.log("Connected to DB."));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 app.use("/scores", scoreRoutes);
 
