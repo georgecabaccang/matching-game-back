65128476bbfdd559aba3d4a1682c70a71f6ad5a5
georgesycabaccang <georgesycabaccang@gmail.com>
2023-12-01 18:09:32 +0800

    Change position of cors to line 20, be the first one before express.

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,9 @@ database.on("error", (error) => {
 database.once("open", () => console.log("Connected to DB."));
 // -----------------------------------------------------------------
 
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 app.get("/", (request: Request, response: Response) => {
     response.send("Test");
